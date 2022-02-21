2d8d44308bb569fef103dbd68eb2f88ee52cd6ad
Ivan Ivanov <indagoo@gmail.com>
2022-02-21 16:19:49 +0500

    User Model

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 import config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object("config.BaseConfig")
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class BaseConfig(object):
-    SECRET_KEY= os.environ.get('SECRET_KEY') or 'TheMostSecretKeyEver'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "TheMostSecretKeyEver"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URI"
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
